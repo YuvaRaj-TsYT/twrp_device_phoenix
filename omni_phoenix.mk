b004b4f7b2ff7cddce34a15c0553a132444a8bd7
YuvaRaj <yuvarajv952@gmail.com>
2020-03-10 15:50:37 +0530

    Update omni_phoenix.mk

--- omni_phoenix.mk
+++ omni_phoenix.mk
@@ -1,18 +1,21 @@
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-PRODUCT_COPY_FILES += device/xiaomi/phoenix/prebuilt/zImage:kernel
-
 PRODUCT_DEVICE := phoenix
 PRODUCT_NAME := omni_phoenix
-PRODUCT_BRAND := Poco
+PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := phoenix
-PRODUCT_MANUFACTURER := iaomi
-
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/etc/recovery.fstab:root/etc/recovery.fstab \
-    $(LOCAL_PATH)/recovery/root/sbin/permissive.sh:root/sbin/permissive.sh \
-    $(LOCAL_PATH)/recovery/root/init.rc:root/init.rc \
-    $(LOCAL_PATH)/recovery/root/init.recovery.hardware.rc:root/init.recovery.hardware.rc \
-    $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
-    $(LOCAL_PATH)/recovery/root/ueventd.rc:root/ueventd.rc
+PRODUCT_MANUFACTURER := xiaomi
